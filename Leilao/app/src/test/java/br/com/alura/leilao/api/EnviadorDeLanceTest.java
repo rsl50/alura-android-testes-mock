be11ab60a456a7508f6d5105d6a45774e28216bc
Robson Lima <rsl.robson@gmail.com>
2020-07-16 12:01:29 -0300

    Implementados testes para QuandoOUsuarioDoUltimoLanceDerNovoLance, QuandoFalharEnvioDeLanceParaAPI e QuandoEnviarLanceParaAPIComSucesso

--- Leilao/app/src/test/java/br/com/alura/leilao/api/EnviadorDeLanceTest.java
+++ Leilao/app/src/test/java/br/com/alura/leilao/api/EnviadorDeLanceTest.java
@@ -2,14 +2,16 @@ package br.com.alura.leilao.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
 import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
 import br.com.alura.leilao.api.retrofit.client.RespostaListener;
 import br.com.alura.leilao.exception.LanceMenorQueUltimoLanceException;
+import br.com.alura.leilao.exception.LanceSeguidoDoMesmoUsuarioException;
 import br.com.alura.leilao.exception.UsuarioJaDeuCincoLancesException;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
@@ -18,6 +20,7 @@ import br.com.alura.leilao.ui.dialog.AvisoDialogManager;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -41,7 +44,7 @@ public class EnviadorDeLanceTest {
     public void deve_MostrarMensagemDeFalha_QuandoLanceForMenorQueUltimoLance() {
         EnviadorDeLance enviador = new EnviadorDeLance(client, listener, manager);
 
-        doThrow(LanceMenorQueUltimoLanceException.class).when(leilao).propoe(ArgumentMatchers.any(Lance.class));
+        doThrow(LanceMenorQueUltimoLanceException.class).when(leilao).propoe(any(Lance.class));
 
         enviador.envia(leilao, new Lance(new Usuario("Fran"), 100));
 
@@ -54,11 +57,64 @@ public class EnviadorDeLanceTest {
         EnviadorDeLance enviador = new EnviadorDeLance(client, listener, manager);
 
         //Teste direto da exceção sem necessidade de simular vários lances
-        doThrow(UsuarioJaDeuCincoLancesException.class).when(leilao).propoe(ArgumentMatchers.any(Lance.class));
+        doThrow(UsuarioJaDeuCincoLancesException.class).when(leilao).propoe(any(Lance.class));
 
         enviador.envia(leilao, new Lance(new Usuario("Robson"), 200));
 
         verify(manager).mostraAvisoUsuarioJaDeuCincoLances();
         verify(client, never()).propoe(any(Lance.class), anyLong(), any(RespostaListener.class));
+    }
+
+    @Test
+    public void deve_MostrarMensagemDeFalha_QuandoOUsuarioDoUltimoLanceDerNovoLance(){
+        EnviadorDeLance enviador = new EnviadorDeLance(client, listener, manager);
+
+        //Teste direto da exceção sem necessidade de simular vários lances
+        doThrow(LanceSeguidoDoMesmoUsuarioException.class).when(leilao).propoe(any(Lance.class));
+
+        enviador.envia(leilao, new Lance(new Usuario("Robson"), 200));
+
+        verify(manager).mostraAvisoLanceSeguidoDoMesmoUsuario();
+        verify(client, never()).propoe(any(Lance.class), anyLong(), any(RespostaListener.class));
+    }
+
+    @Test
+    public void deve_MostraMensagemDeFalha_QuandoFalharEnvioDeLanceParaAPI(){
+        EnviadorDeLance enviador = new EnviadorDeLance(client, listener, manager);
+
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                RespostaListener<Void> argument = invocation.getArgument(2);
+                argument.falha("");
+
+                return null;
+            }
+        }).when(client).propoe(any(Lance.class), anyLong(), any(RespostaListener.class));
+
+        enviador.envia(new Leilao("Computador"), new Lance(new Usuario("Robson"), 200));
+
+        verify(manager).mostraToastFalhaNoEnvio();
+        verify(listener, never()).processado(new Leilao("Computador"));
+    }
+
+    @Test
+    public void deve_NotificarLanceProcessado_QuandoEnviarLanceParaAPIComSucesso(){
+        EnviadorDeLance enviador = new EnviadorDeLance(client, listener, manager);
+
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                RespostaListener<Void> argument = invocation.getArgument(2);
+                argument.sucesso(any(Void.class));
+
+                return null;
+            }
+        }).when(client).propoe(any(Lance.class), anyLong(), any(RespostaListener.class));
+
+        enviador.envia(new Leilao("Computador"), new Lance(new Usuario("Robson"), 200));
+
+        verify(listener).processado(any(Leilao.class));
+        verify(manager, never()).mostraToastFalhaNoEnvio();
     }
 }
